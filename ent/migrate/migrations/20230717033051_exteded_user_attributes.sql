b0d9f040d3deee6da961623998ec2e4c3752c1a2
findridoy <findridoy@gmail.com>
2023-07-26 11:18:30 +0600

    init seed

--- ent/migrate/migrations/20230717033051_exteded_user_attributes.sql
+++
@@ -1,2 +0,0 @@
--- Modify "users" table
-ALTER TABLE `users` MODIFY COLUMN `username` varchar(50) NOT NULL, ADD COLUMN `first_name` varchar(50) NOT NULL, ADD COLUMN `last_name` varchar(50) NOT NULL, ADD COLUMN `phone_number` varchar(255) NULL, ADD COLUMN `email` varchar(255) NULL, ADD COLUMN `role` enum('SUPERADMIN','ADMIN','CASHIER','CUSTOMER') NOT NULL;
--- ent/migrate/migrations/20230724083100_added_user_to_role_link.sql
+++
@@ -1,6 +0,0 @@
--- Modify "permissions" table
-ALTER TABLE `permissions` DROP COLUMN `name`, ADD COLUMN `title` varchar(50) NOT NULL, ADD COLUMN `value` varchar(50) NOT NULL, ADD COLUMN `group` varchar(50) NULL;
--- Modify "roles" table
-ALTER TABLE `roles` DROP COLUMN `name`, ADD COLUMN `title` varchar(50) NOT NULL;
--- Modify "users" table
-ALTER TABLE `users` DROP COLUMN `has_permission`, ADD INDEX `users_roles_users` (`role_id`), ADD CONSTRAINT `users_roles_users` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION;
